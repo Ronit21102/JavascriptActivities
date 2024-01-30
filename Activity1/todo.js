070c571e0c2ffc622dd7067c9753b0deebc028c4
Ronit Roushan <ronit21102@gmail.com>
2024-01-30 17:16:18 +0530

    added Enter button feature

--- Activity1/todo.js
+++ Activity1/todo.js
@@ -3,6 +3,7 @@
 const taskList = document.getElementById('taskList');
 // The input we are writing
 const taskInput = document.getElementById('taskInput')
+
 function addTask(){
     
 
@@ -17,8 +18,13 @@ taskList.appendChild(newElement)
  
 }
 
-
-//adding on click
+// Added Enter for adding task
+document.getElementById('taskInput').addEventListener('keypress', function(event) {
+    if (event.code === 'Enter') {
+        // Call the addTask function on Enter key press
+        addTask();
+    }
+});
 const time = document.getElementById("time");
 
 time.innerHTML=`${new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month:"short", day: "numeric" })}`
