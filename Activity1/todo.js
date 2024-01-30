62d39781687cf5aa232c74dcfc3dd37b79532341
Ronit Roushan <ronit21102@gmail.com>
2024-01-30 22:23:21 +0530

    Finally

--- Activity1/todo.js
+++ Activity1/todo.js
@@ -1,11 +1,17 @@
+const time = document.getElementById("time");
+
+time.innerHTML=`${new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month:"short", day: "numeric" })}`
 
 
-const taskList = document.getElementById('taskList');
-// The input we are writing
 const taskInput = document.getElementById('taskInput')
+const taskList = document.getElementById('taskList');
+const button = document.getElementById("button")
 
+
+
+
+button.addEventListener('click',addTask);
 function addTask(){
-    
 
 const text = taskInput.value;
 
@@ -16,11 +22,17 @@ if(text ===''){
 else{
 taskInput.value=''
 
-const newElement = document.createElement('div')
-newElement.innerHTML =`<li class="list-group-item mb-3">${text}</li>`
-taskList.appendChild(newElement)
+let li = document.createElement('li')
+li.innerHTML = text;
+taskList.appendChild(li)
+
+li.className = ["list-group-item mb-3 "]
+
+let span = document.createElement("span");
+        span.innerHTML = "x";
+        li.appendChild(span);
+        saveData();
 }
- 
 }
 
 // Added Enter for adding task
@@ -30,10 +42,38 @@ document.getElementById('taskInput').addEventListener('keypress', function(event
         addTask();
     }
 });
-const time = document.getElementById("time");
 
-time.innerHTML=`${new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month:"short", day: "numeric" })}`
+taskList.addEventListener("click", (e)=>{
+
+    if(e.target.tagName === "LI"){
+
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        console.log(e.target.parentElement);
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
 
 
+// Saving the data for list retention from refresh
+
+function saveData(){
+    localStorage.setItem("data",taskList.innerHTML);
+}
+
+
+function showTask(){
+    const savedData = localStorage.getItem("data")
+
+    if(savedData){
+        taskList.innerHTML = savedData
+    }
+}
+
+
+
+window.addEventListener("load",showTask());
 
 
